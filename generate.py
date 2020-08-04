7e9dfdaf05059b3fff237a8619b6b4fb187570e7
ridiculousfish <rf@fishshell.com>
2020-08-04 12:20:58 -0700

    Allow EastAsianWidth specifiers to trump WIDTH_WIDENED_IN_9
    Prior to this change, we marked emoji that were introduced before Unicode 9
    as widened in 9. However some emoji were left narrow in 9; for example
    regional specifiers, hot pepper U+1F336, some others.
    
    Change the output so that characters which are neutral in EastAsianWidth.txt
    are kept as width 1.

--- generate.py
+++ generate.py
@@ -330,11 +330,22 @@ def set_emoji_widths(emoji_data_lines, cps):
             # Don't consider <=1F000 values as emoji. These can only be made
             # emoji through the variation selector which interacts terribly
             # with wcwidth().
+            if cp < 0x1F000:
+                continue
+
             # Skip codepoints that have a version of 0.0 as they were marked
             # in the emoji-data file as reserved/unused:
-            if cp >= 0x1F000:
-                if version > 1.0:
-                    cps[cp].width = 2 if version >= 9.0 else WIDTH_WIDENED_IN_9
+            if version <= 1.0:
+                continue
+
+            # Skip codepoints that are explicitly not wide.
+            # For example U+1F336 ("Hot Pepper") renders like any emoji but is
+            # marked as neutral in EAW so has width 1 for some reason.
+            if cps[cp].width == 1:
+                continue
+
+            # If this emoji was introduced before Unicode 9, then it was widened in 9.
+            cps[cp].width = 2 if version >= 9.0 else WIDTH_WIDENED_IN_9
 
 
 def set_hardcoded_ranges(cps):
